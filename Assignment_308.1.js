f3169df5b8a96801d3dae590e58756679d9d2f3f
StephOg <stephanieogwo@Stephanies-MBP.lan>
2024-05-21 11:41:43 -0400

    Check 6 is successful

--- Assignment_308.1.js
+++ Assignment_308.1.js
@@ -1,6 +1,6 @@
 // The initial numbers that must be verified.
-const n1 = 9;
-const n2 = 16;
+const n1 = 10;
+const n2 = 15;
 const n3 = 20;
 const n4 = 5;
 
@@ -47,3 +47,4 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 // The below is my code going forward
 // Check 6: All numbers are divisible by 5 - I'll use mod operand to achieve this
 const divByFive = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0;
+console.log("divByFive: " + divByFive);
