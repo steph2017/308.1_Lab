66c3c8ef8e9698569c4605f836aa2ca351bdb29f
StephOg <stephanieogwo@Stephanies-MBP.lan>
2024-05-21 11:39:27 -0400

    Check 6 1st draft

--- Assignment_308.1.js
+++ Assignment_308.1.js
@@ -1,6 +1,6 @@
 // The initial numbers that must be verified.
-const n1 = 10;
-const n2 = 15;
+const n1 = 9;
+const n2 = 16;
 const n3 = 20;
 const n4 = 5;
 
@@ -43,3 +43,7 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
     ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) &&
     !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) &&
     (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+// The below is my code going forward
+// Check 6: All numbers are divisible by 5 - I'll use mod operand to achieve this
+const divByFive = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0;
